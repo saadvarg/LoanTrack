bdc7ca5f1360812515ce0e50f7ee420e8b0a5a79
Saad <elmouataz.saad@gmail.com>
2026-04-18 23:43:50 +0100

    Initial Commit

---
+++ LoanTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoanTrack
+//
+//  Created by Saad EL Mouataz on 18/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoanTrack/LoanTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanTrackApp.swift
+//  LoanTrack
+//
+//  Created by Saad EL Mouataz on 18/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct LoanTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
